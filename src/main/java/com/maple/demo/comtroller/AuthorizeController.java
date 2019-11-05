ac9a343588483978d909cb041d808278ff4babf7
mapleKirito <myzhouye@gmail.com>
2019-11-05 23:00:53 +0800

    家里 把相关ID等参数数据提取到application.properties中 并利用Value注解引入

--- src/main/java/com/maple/demo/comtroller/AuthorizeController.java
+++ src/main/java/com/maple/demo/comtroller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.maple.demo.entity.AccessTokenEntity;
 import com.maple.demo.entity.GithubUserEntity;
 import com.maple.demo.utils.GithubUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,25 @@ public class AuthorizeController {
     @Autowired
     private GithubUtil githubUtil;
 
+    @Value("${Github.client.id}")
+    private String clientId;
+    @Value("${Github.client.secret}")
+    private String clientSecret;
+    @Value("${Github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
+
+
+
         AccessTokenEntity accessTokenEntity = new AccessTokenEntity();
-        accessTokenEntity.setClient_id("91069c8f973100bbe46a");
+        accessTokenEntity.setClient_id(clientId);
         accessTokenEntity.setCode(code);
         accessTokenEntity.setState(state);
-        accessTokenEntity.setRedirect_uri("http://localhost:8080/callback");
-        accessTokenEntity.setClient_secret("e3b1d565788fe83437ae0cd0ade6a5b9b6454282");
+        accessTokenEntity.setRedirect_uri(redirectUri);
+        accessTokenEntity.setClient_secret(clientSecret);
         String accessToken = githubUtil.getAccessToken(accessTokenEntity);
         GithubUserEntity user = githubUtil.getUser(accessToken);
         System.out.println(user.getName());
